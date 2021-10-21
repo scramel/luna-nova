8a577e1d3d4a7280550318e866685f4b423b1096
Orion <omelgarejo@vikua.com>
2021-10-20 20:17:37 -0400

    Fixed config files

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:scramel/works.git main:main
+git push -f git@github.com:scramel/works.git master:gh-pages
 
 cd -
