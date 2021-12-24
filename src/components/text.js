a47e9fbb3fd1bd1d49cf0a82a6029148fb5a94cb
Orion <omelgarejo@vikua.com>
2021-12-24 09:53:12 -0400

    eng tl

--- src/components/text.js
+++ src/components/text.js
@@ -1,78 +1,78 @@
 export default (vm) => [
     [[ // ACT 0
-        `<p>${ vm.$t('lunanova.ch1.1') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.2') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.3') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.1') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.2') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.3') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.4') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.5') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.4') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.5') }.</p>`,
     ], [
-        `<p><i>«${ vm.$t('lunanova.ch1.6') }»</i>, <i>«${ vm.$t('lunanova.ch1.7') }»</i>, <i>«${ vm.$t('lunanova.ch1.8') }»</i>, <i>«${ vm.$t('lunanova.ch1.9') }»</i></p>`
+        `<p><i>${ vm.$t('lunanova.ch1.6') }</i>, <i>${ vm.$t('lunanova.ch1.7') }</i>, <i>${ vm.$t('lunanova.ch1.8') }</i>, <i>${ vm.$t('lunanova.ch1.9') }</i>.</p>`
     ], [
-        `<p>${ vm.$t('lunanova.ch1.10') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.11') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.10') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.11') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.12') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.13') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.12') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.13') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.14') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.15') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.14') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.15') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.16') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.17') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.16') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.17') }.</p>`,
     ], [
         `<p>${ vm.$t('lunanova.ch1.18') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.19') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.19') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.20') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.21') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.20') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.21') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.22') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.23') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.22') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.23') }.</p>`,
     ], [
-        `<p>"<i>${ vm.$t('lunanova.ch1.24') }</i>". ${ vm.$t('lunanova.ch1.25') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.26') }</p>`,
+        `<p><i>${ vm.$t('lunanova.ch1.24') }</i>. ${ vm.$t('lunanova.ch1.25') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.26') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.27') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.28') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.27') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.28') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.29') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.29') }.</p>`,
         `<p>${ vm.$t('lunanova.ch1.30') }</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.31') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.32') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.33') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.31') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.32') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.33') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.34') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.35') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.34') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.35') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.36') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.36') }.</p>`,
         `<p>${ vm.$t('lunanova.ch1.37') }</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.38') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.39') }</p>`
+        `<p>${ vm.$t('lunanova.ch1.38') } <i>${ vm.$t('lunanova.ch1.39') }</i>.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.40') }.</p>`
     ], [
-        `<p>${ vm.$t('lunanova.ch1.40') }</p>`,
         `<p>${ vm.$t('lunanova.ch1.41') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.42') } <i>${ vm.$t('lunanova.ch1.43') }</i>. ${ vm.$t('lunanova.ch1.44') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.42') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.43') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.45') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.46') }.</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.44') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.45') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.46') }</p>`,
-    ], [
-        `<p>${ vm.$t('lunanova.ch1.47') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.48') }</p>`,
-    ], [
+        `<p>${ vm.$t('lunanova.ch1.47') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.48') }.</p>`,
         `<p>${ vm.$t('lunanova.ch1.49') }</p>`,
-        `<p>${ vm.$t('lunanova.ch1.50') }</p>`,
     ], [
-        `<p>${ vm.$t('lunanova.ch1.51') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.50') }.</p>`,
+        `<p>${ vm.$t('lunanova.ch1.51') }.</p>`,
+    ], [
         `<p>${ vm.$t('lunanova.ch1.52') }</p>`,
-    ], [
         `<p>${ vm.$t('lunanova.ch1.53') }</p>`,
+    ], [
         `<p>${ vm.$t('lunanova.ch1.54') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.55') }</p>`,
+    ], [
+        `<p>${ vm.$t('lunanova.ch1.56') }</p>`,
+        `<p>${ vm.$t('lunanova.ch1.57') }.</p>`,
     ]], [[ // ACT 1
         `<p>${ vm.$t('lunanova.ch1.55') }</p>`,
     ], [
